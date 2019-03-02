c44ef7b04bb85e4de4a9ea2e2588dca56f296ad1
leonel <leonel.araujo27@gmail.com>
2019-03-02 20:37:25 +0000

    infer geolocation from ip on standby as api used is deprecated

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import argparse
 from src.print import print_sightings
-from src.find import get_sightings, find_location_by_city_name, find_location_by_gps_coordinates
+from src.find import get_sightings, find_location_by_city_name, find_location_by_gps_coordinates, find_location_from_ip
 
 parser = argparse.ArgumentParser(description='Get next International Space Station sightings in your city.')
 parser.add_argument('--city', '-c', metavar='Braga', type=str, help='city')
 parser.add_argument('--coordinates', '-g', metavar='42.5,-8.4', type=str, help='comma-separated gps coordinates. \'latitude,longitude\'')
-# parser.add_argument('--infer', '-i', type=str, help='infer location from ip')
+# parser.add_argument('--infer', '-i', action='store_true', help='infer location from ip')
 args = parser.parse_args()
 location = None
 
@@ -19,6 +19,10 @@ if args.coordinates is not None:
     location = find_location_by_gps_coordinates(latitude, longitude)
     print('The closest location to {latitude},{longitude} found was {city}'
           .format(latitude=latitude, longitude=longitude, city=location['full_location']))
+
+if args.infer is not None:
+    location = find_location_from_ip()
+    print('The closest location found was {city}'.format(city=location['full_location']))
 
 sighting_list = get_sightings(location.country, location.region, location.city)
 print_sightings(location.city, sighting_list)
--- src/find.py
+++ src/find.py
@@ -12,19 +12,19 @@ df_locations = pd.DataFrame(locations, columns=['full_location', 'latitude', 'lo
 
 
 def get_sightings(country, region, city):
+    # Fetch rss feed of the location given
     rss = 'https://spotthestation.nasa.gov/sightings/xml_files/{}_{}_{}.xml'.format(
         country, region, city
     )
     r = requests.get(rss)
-    xml = BeautifulSoup(r.text, features="html.parser")
-    items = xml.find_all('item')
 
+    # Parse the information from the received feed
+    xml = BeautifulSoup(r.text, features='html.parser')
+    items = xml.find_all('item')
     sightings = []
     for item in items:
-        # Get title of sighting
+        # Discarding any sighting events that aren't of ISS
         title = item.find('title').contents[0][11:]
-
-        # Discarding any other sighting events
         if title != 'ISS Sighting':
             continue
 
@@ -38,24 +38,25 @@ def get_sightings(country, region, city):
         departure = re.search(r'Departure:\s(.*)\s<br/>', info).groups()[0]
 
         sightings.append({
-            "sighting": item.find('title').contents[0][11:],
-            "timestamp": datetime.datetime.strptime(date + time, '%A %b %d, %Y %I:%M %p'),
-            "duration": duration,
-            "maximum_elevation": maximum_elevation,
-            "approach": approach,
-            "departure": departure
+            'sighting': item.find('title').contents[0][11:],
+            'timestamp': datetime.datetime.strptime(date + time, '%A %b %d, %Y %I:%M %p'),
+            'duration': duration,
+            'maximum_elevation': maximum_elevation,
+            'approach': approach,
+            'departure': departure
         })
 
     # Remove sightings that have already occurred
-    sightings = [s for s in sightings if s["timestamp"] > datetime.datetime.now()]
-    sightings = sorted(sightings, key=lambda s: s["timestamp"])
+    sightings = [s for s in sightings if s['timestamp'] > datetime.datetime.now()]
+    sightings = sorted(sightings, key=lambda s: s['timestamp'])
     return sightings
 
 
 def find_location_from_ip():
     r = requests.get('http://freegeoip.net/json')
     j = json.loads(r.text)
-    return j['latitude'], j['longitude']
+    [latitude, longitude] = j['latitude'], j['longitude']
+    return find_location_by_gps_coordinates(latitude, longitude)
 
 
 def find_location_by_city_name(city=None):
@@ -71,7 +72,8 @@ def find_location_by_gps_coordinates(latitude, longitude):
     if latitude is None or longitude is None:
         raise BaseException('No location source given as parameter')
 
-    # filter the locations with < 1 latitude/longitude degrees
+    # Filter the locations that are at least 1 lat/lon decimal degree away so we only
+    # calculate the haversine distance of the relevant locations
     matching_locations = df_locations.drop(df_locations[~(
         (df_locations.latitude < latitude + 1) &
         (df_locations.latitude > latitude - 1) &
